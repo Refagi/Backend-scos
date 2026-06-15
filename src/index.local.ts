d0d7f64ac46f4f550288bb40ee21e39c3e17d41c
Refagi <renogivaro@gmail.com>
2026-06-15 10:15:42 +0700

    fix: script tsc-alias

--- src/index.local.ts
+++
@@ -1,9 +0,0 @@
-import app from '@/app'
-import prisma from '../prisma/client'
-import { logger } from '@/config/logger'
-
-prisma.$connect()
-  .then(() => logger.info('Connected to Database'))
-  .catch((err) => logger.error(err))
-
-export default app
